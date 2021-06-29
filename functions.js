79f584f81b21193ad85291cbbd43d6f4331e375d
CarlosVelilla <carlosvelillaf@hotmail.com>
2021-06-29 17:39:25 +0200

    Rest of JS functions and start of visual improvements

--- functions.js
+++ functions.js
@@ -1,13 +1,13 @@
 // DARK MODE
 
-const checkbox = document.getElementById("checkbox")
-
-checkbox.addEventListener("click", changeTheme)
+const CHECKBOX = document.getElementById("checkbox")
+CHECKBOX.addEventListener("click", changeTheme)
 
 function changeTheme() {
   document.getElementById("calculator").classList.toggle("dark")
   document.getElementById("equal").classList.toggle("dark")
   document.getElementById("displayNums").classList.toggle("dark")
+  document.getElementById("decimal").classList.toggle("dark")
   document.getElementById("historyButton").classList.toggle("dark")
   let operator = document.getElementsByClassName("operator")
   for (let i = 0; i < operator.length; i++) {
@@ -18,6 +18,45 @@ function changeTheme() {
     btnNumbers[i].classList.toggle("dark")
   }
 }
+
+// OPEN/CLOSE CALCULATOR WINDOW
+
+let container = document.getElementById("container")
+let calculator = document.getElementById("calculator")
+
+let calculatorIcon = document.getElementById("calculatorLogoBar")
+calculatorIcon.addEventListener("click", openWindow)
+
+function openWindow() {
+  calculator.classList.remove("hidden")
+}
+
+let closeBtn = document.getElementById("closeWindow")
+closeBtn.addEventListener("click", closeWindow)
+
+function closeWindow() {
+  calculator.classList.add("hidden")
+}
+
+// MINIMIZE WINDOW
+
+// TODO REVISAR TODO ESTO PARA ANIMACIÓN / AÑADIR INDICADOR A LA BARRA DE SISTEMA DE ELEMENTO MINIMIZADO
+
+// let minimizeBtn = document.getElementById("minimizeWindow")
+// minimizeBtn.addEventListener("click", minimizeWindow)
+
+// function minimizeWindow() {
+//   calculator.classList.add("hidden")
+//   openBtn.classList.add("showOpenButton")
+// }
+
+// let openBtn = document.getElementById("openWindow")
+// openBtn.addEventListener("click", reopenWindow)
+
+// function reopenWindow() {
+//   document.getElementById("calculator").classList.remove("hidden")
+//   openBtn.classList.remove("showOpenButton")
+// }
 
 // LOCATE OPERATION HTML
 
@@ -68,7 +107,7 @@ clearBtn.addEventListener("click", clearOperation)
 
 function clearOperation() {
   operation.innerHTML = 0
-  history.innerHTML = " "
+  history.innerHTML = ""
 }
 
 // CHANGE OPERATOR PLUSMINUS ± FUNCTION
@@ -82,4 +121,31 @@ function changeOperatorPlusMinus() {
   } else if (operation.innerHTML.slice(-1) === "-") {
     console.log("es un menos")
   }
+}
+
+// OPERATION LOG
+
+let operationsAccomplished = []
+
+// EQUAL FUNCTION
+
+let equal = document.getElementById("equal")
+equal.addEventListener("click", solveOperation)
+
+function solveOperation() {
+  let currentOperation = operation.innerHTML
+  history.innerHTML = currentOperation
+  let result = eval(operation.innerHTML)
+  operation.innerHTML = result
+  // AÑADIMOS LA OPERACIÓN AL LOG
+  operationsAccomplished.push({operation: currentOperation, result: result})
+}
+
+// HISTORY CONSOLE LOG
+
+let historyBtn = document.getElementById("historyButton")
+historyBtn.addEventListener("click", consoleLogOperations)
+
+function consoleLogOperations() {
+  console.log(operationsAccomplished)
 }
